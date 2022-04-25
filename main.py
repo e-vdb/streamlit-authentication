dff4aa7549da3927c70c5456ea66a23ca51c5c44
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-04-25 14:00:55 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+from authentication.auth import UsernameDatabase
+
+
+load_dotenv()
+
+
+db = UsernameDatabase(username=os.getenv('USERNAME'), password=os.getenv('PASSWORD'), host=os.getenv('HOST'),
+                      port=os.getenv('PORT'), database=os.getenv('DATABASE'))
+db.connect_to_db()
