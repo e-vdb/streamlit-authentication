d2986ae307bb8a2757826fed3162c6ad5aa5451a
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-04-25 15:35:21 +0200

    Defined class to manage streamlit web app for demo

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 from dotenv import load_dotenv
 import os
-from authentication import UsernameDatabase
-
+from authentication import StreamlitAuthenticator
+import streamlit as st
+from streamlit import cli as stcli
+import sys
 
 load_dotenv()
 
 
-db = UsernameDatabase(username=os.getenv('USERNAME'), password=os.getenv('PASSWORD'), host=os.getenv('HOST'),
-                      port=os.getenv('PORT'), database=os.getenv('DATABASE'))
-db.connect_to_db()
+class App:
+    def __init__(self):
+        self.authenticator = StreamlitAuthenticator(username=os.getenv('USERNAME'), password=os.getenv('PASSWORD'),
+                                                    host=os.getenv('HOST'), port=os.getenv('PORT'),
+                                                    database=os.getenv('DATABASE'))
+
+    def authentication(self):
+        self.authenticator.authentication(main_app_fct_to_show=self.show, app_name='demo')
+
+    @staticmethod
+    def show():
+        st.markdown("Successful connection to the streamlit web app.")
+
+
+def main():
+    front = App()
+    front.authentication()
+
+
+if __name__ == '__main__':
+    if st._is_running_with_streamlit:
+        main()
+    else:
+        sys.argv = ["streamlit", "run", sys.argv[0]]
+        sys.exit(stcli.main())
