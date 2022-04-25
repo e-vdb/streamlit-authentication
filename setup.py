5ba36f31f8d8e4c5a55ad110685fdc47d3fcb628
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-04-25 14:44:14 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name="streamlit-authentication",
+
+    version="1.0.0",
+
+    author="e-vdb",
+
+    author_email="emelinevanderbeken@gmail.com",
+
+    description="Manage a database to save user and password that can access streamlit web apps",
+
+    long_description=long_description,
+
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/e-vdb/streamlit-authentication",
+
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+
+        "Programming Language :: Python :: 3",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Operating System :: OS Independent",
+
+    ],
+    install_requires=["pandas>=1.4.2", "SQLAlchemy>=1.4.35", "psycopg2>=2.9.3", "passlib>=1.7.4"],
+    python_requires='>=3.9',
+
+)
